4b8ed7c5d24a5553efc87e603e5f4c87d8a6246b
shinkeonkim <singun11@naver.com>
2020-06-12 21:36:27 +0900

    feat:gameStart(), showtime(), findDog()

--- src/dog-20191564-김신건.js
+++ src/dog-20191564-김신건.js
@@ -1,7 +1,80 @@
-function mouseup_shadow(obj) {
-    $(obj).addClass('shadow-lg bg-white rounded');
+var leftDog = 0; // 남은 강아지 수
+var leftGameTime = 0; // 남은 시간
+var leftGameTimer; // 남은 시간 타이머
+
+var leftShowTime = 10;
+var showTimer;
+
+function init() {
+    // 이벤트리스너 초기화
+    eventInit();
+
 }
 
-function mouseout_shadow(obj) {
-    $(obj).removeClass('shadow-lg bg-white rounded');
+function eventInit() {
+    $("#game-start").click(function (e) {
+        if(gameStart()) {
+            $(this).hide();
+        }
+    });
+
+    $(".egg").mouseenter(function () { 
+        $(this).addClass('shadow-lg');
+    });
+
+    $(".egg").mouseleave(function () { 
+        $(this).removeClass('shadow-lg');
+    });
+
+    $(".nav-item").mouseenter(function () { 
+        $(this).addClass('shadow-lg');
+    });
+
+    $(".nav-item").mouseleave(function () { 
+        $(this).removeClass('shadow-lg');
+    });
+}
+
+function gameStart() {
+    
+    
+    // 사용자가 입력한 `남은 시간 수` 저장
+    leftGameTime = $("#left-time").val();
+    
+    if(leftGameTime == " " || isNaN(leftGameTime) || leftGameTime >= 20 || leftGameTime <= 0) {
+        alert("게임 시간은 1 ~ 19 까지의 숫자만 입력해주세요.");
+        return false;
+    }
+
+    $("#left-time-label").val("남은 시간: ");
+    
+    // 사용자가 입력한 `찾을 강아지 수` 화면에 표시 및 저장
+    leftDog = $("#left-dog-select").find("option:selected").val();
+    $("#left-dog-count").html('<h4 class = "col-4" id = "left-dog-count">' + leftDog + '</h4>');
+    
+    // 10초동안 개의 위치 보여주기
+    showTime();
+
+    return true;
+}
+
+function showTime() {
+    alert("개의 위치를 기억하세요!");
+
+    $("#left-time-count").html('<h4 class = "col-4" id = "left-time-count">' + leftShowTime + '</h4>');
+    
+    showTimer = setInterval(function() {
+        leftShowTime-=1;
+        $("#left-time-count").html('<h4 class = "col-4" id = "left-time-count">' + leftShowTime + '</h4>');
+        if(leftShowTime <= 0) {
+            clearInterval(showTimer);
+            findDog();
+        }
+    }, 1000);
+}
+
+function findDog() {
+
+    alert("찾으세요!");
+    
 }
