39291a947c93bdb1c11d095dd6eb95b5e13a6658
shinkeonkim <singun11@naver.com>
2020-06-13 15:14:12 +0900

    feat:audio 기능

--- src/dog-20191564-김신건.js
+++ src/dog-20191564-김신건.js
@@ -8,12 +8,18 @@ var isStarted = false;
 var isfound = false;
 var failCount = 0;
 
+var backGroundMusic;
+var clockSound;
+var successSound;
+var tadaSound;
+
 function init() {
     // 전역변수 초기화
     varInit();
     // 이벤트리스너 초기화
     eventInit();
 
+    mediaInit();
 }
 
 function varInit() {
@@ -52,15 +58,51 @@ function eventInit() {
     $(".egg").click(function() {
         dogClicked(this);
     });
+
+    // https://developers.google.com/web/updates/2017/09/autoplay-policy-changes
+    // 크롬의 autoplay 정책 변경으로 크롬에서는 사용자에게 클릭 등의 이벤트가 일어나야 노래가 재생되게 바뀌었음.
+    // 이에 따라 추가한 이벤트                
+    $("#music-button").click(function (e) { 
+        backGroundMusic.play();
+        $(this).hide();
+    });
 }
 
 
+function mediaInit() {
+
+    backGroundMusic = new Audio();
+    backGroundMusic.src = "../media/background.mp3";
+    backGroundMusic.loop = true;
+    backGroundMusic.play();
+
+    clockSound = new Audio("../media/clock.mp3");
+    successSound = new Audio("../media/ending.mp3");
+    tadaSound = new Audio("../media/tada.mp3");
+}
+
+// 사용자가 빠르게 누르면, 하나의 객체로 정의되었을 경우에는 사운드가 들리지 않을 수 있음.
+// 따라서, badSound와 goodSound는 그때마다 새로운 객체로 선언하여 play해야함.
+function badSoundPlay() {
+
+    var badSound = new Audio("../media/bad.mp3");
+    badSound.play();
+}
+
+function goodSoundPlay() {
+    var goodSound = new Audio("../media/chimes.mp3");
+    goodSound.play();
+}
+
+// min ~ max-1 범위의 난수를 하나 생성한다.
 function getRandomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+// leftDog 개수만큼의 중복되지 않는 난수 목록을 생성한다.
+// 재귀함수 방식으로 작동한다.
 function getRandomNumbers(arr) {
     if (!arr) {
         arr = [];
@@ -78,8 +120,7 @@ function getRandomNumbers(arr) {
     }
 }
 
-
-
+// 게임 시작 버튼이 눌렀을 때 실행되는 함수
 function gameStart() {
     
     // 사용자가 입력한 `남은 시간 수` 저장
@@ -104,6 +145,7 @@ function gameStart() {
     // 10초동안 개의 위치 보여주기
     showTime();
 
+    //게임이 정상적으로 실행되었으므로, true를 return한다.
     return true;
 }
 
@@ -114,10 +156,12 @@ function showDog() {
     }
 }
 
+// 특정 id의  개 이미지를 보여준다.
 function showCertainDog(num) {
     $("#egg-" + num).attr("src", "../media/img2.gif");
 }
 
+// 실패 시에, 찾지 못하고 남아있는 강아지만, 빨간색 테두리와 함께 보여준다.
 function showFailDog() {
     for(var i = 0; i < leftDog; i++) {
         $("#egg-" + dogNumberList[i]).attr("src", "../media/img2.gif");
@@ -141,10 +185,13 @@ function showTime() {
     
     showTimer = setInterval(function() {
         leftShowTime-=1;
+        if(leftShowTime<=5) {
+            clockSound.play();
+        }
         $("#left-time-count").html('<h4 class = "col-4" id = "left-time-count">' + leftShowTime + '</h4>');
         if(leftShowTime <= 0) {
             clearInterval(showTimer);    
-           hideDog();
+            hideDog();
             findDog();
         }
     }, 1000);
@@ -158,15 +205,11 @@ function findDog() {
     $("#left-time-count").html('<h4 class = "col-4" id = "left-time-count">' + leftGameTime + '</h4>');
     
     gameTimer = setInterval(function() {
-        leftGameTime-=1;
+        clockSound.play();
+        
         $("#left-time-count").html('<h4 class = "col-4" id = "left-time-count">' + leftGameTime + '</h4>');
         if(leftGameTime <= 0 && !isfound) {
-            clearInterval(gameTimer);
-            setGameMsg("실패");
-            showFailDog();
-            $("#fail-msg").css("visibility", "visible");
-            init();  
-            setTimeout(regame, 1500);
+            failGame();
         }
     }, 1000);
 }
@@ -177,10 +220,12 @@ function dogClicked(obj) {
         var ret = dogNumberList.indexOf(1*clickedDogNumber);
         if(ret < 0) {
             failCount++;
+            badSoundPlay();
             $("#fail-count").text(failCount);
         }
         else {
             leftDog--;
+            goodSoundPlay();
             $("#left-dog-count").html('<h4 class = "col-4" id = "left-dog-count">' + leftDog + '</h4>');
             dogNumberList.splice(ret,1);
             showCertainDog(clickedDogNumber);
@@ -191,13 +236,32 @@ function dogClicked(obj) {
     }
 
     if(leftDog <=0) {
-        setGameMsg("성공하셨습니다!");
-        clearInterval(gameTimer);
-        iffound = true;
-        init();
-        setTimeout(regame, 1500);
+        successGame();
     }
+
+    if(failCount >5) {
+        failGame();
+    }
+
 }
+
+function successGame() {
+    setGameMsg("성공하셨습니다!");
+    successSound.play();
+    tadaSound.play();
+    clearInterval(gameTimer);
+    iffound = true;
+    setTimeout(regame, 1500);
+}
+
+function failGame() {
+    clearInterval(gameTimer);
+    setGameMsg("실패");
+    showFailDog();
+    $("#fail-msg").css("visibility", "visible");
+    setTimeout(regame, 1500);
+}
+
 
 function regame() {
     clearTimeout(regame);
@@ -208,5 +272,4 @@ function regame() {
 
 function setGameMsg(str) {
     $("#game-msg").text(str);
-
 }
